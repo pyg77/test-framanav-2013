b97a528d4ef5dae26a2624d28913549e3487708d
pyg77 <pyg+github@framasoft.net>
2013-02-21 18:45:20 +0100

    Update framanav/deploy.php

--- framanav/deploy.php
+++ framanav/deploy.php
@@ -40,7 +40,7 @@
 <pre>
  .  ____  .    ____________________________
  |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
+[| <span style="color: #FF0000;">&hearts;   &hearts;</span> |]  | Git Deployment Script v0.1 |
  |___==___|  /              &copy; oodavid 2012 |
               |____________________________|
  
